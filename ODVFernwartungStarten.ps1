a929871d91e67fbac6a9731e5f021914986bd5a9
KresimirUloznik <108215944+KresimirUloznik@users.noreply.github.com>
2024-02-24 20:49:13 +0100

    Add files via upload

---
+++ ODVFernwartungStarten.ps1
@@ -0,0 +1,17 @@
+﻿$FilePath = "C:\Temp"
+$FileName = "ODVFernwartung.exe"
+$URL = "https://get.teamviewer.com/odvfernwartung"
+
+function Download_TeamViewerQS() {
+    $IEBrowser = New-Object -ComObject InternetExplorer.Application
+    $IEBrowser.Navigate($URL)
+    Start-Sleep -Seconds 3
+    $CustomTVQS_URL = $IEBrowser.Document.getElementById('MasterBodyContent_btnRetry').href
+    Start-BitsTransfer -Source $CustomTVQS_URL -Destination $FilePath\$FileName
+}
+
+if(!(Test-Path $FilePath\$FileName)) {
+    New-Item -Path $FilePath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+    Download_TeamViewerQS
+}
+& Start-Process $FilePath\$FileName
